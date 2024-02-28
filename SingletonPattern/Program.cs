4dd056172663ca69d15abe91030baae5a63609dc
elunadanilo <eluna.danilo@gmail.com>
2024-02-28 10:34:24 -0600

    First Commit

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using SingletonPattern;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Accede a la instancia Singleton utilizando su propiedad estática Instance.
+        Singleton singletonInstance = Singleton.Instance;
+
+        // Utiliza los métodos y propiedades de la instancia Singleton.
+        singletonInstance.SomeMethod();
+
+        // Verifica que la instancia sea la misma en diferentes partes del código.
+        Singleton anotherSingletonInstance = Singleton.Instance;
+        Console.WriteLine(singletonInstance == anotherSingletonInstance); // Debería imprimir 'True'.
+
+        Console.ReadLine(); // Para mantener la consola abierta.
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    using System;
+
+    public class Singleton
+    {
+        // Variable estática privada para almacenar la única instancia de la clase.
+        private static Singleton instance;
+
+        // Declara un constructor privado para evitar que se creen instancias de la clase desde fuera de la misma.
+        private Singleton() { }
+
+        // Declara un método estático público que devuelva la única instancia de la clase.
+        public static Singleton Instance
+        {
+            get
+            {
+                // Verifica si la instancia ya ha sido creada; si no, crea una nueva.
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        // Agrega otros métodos y propiedades según sea necesario.
+        public void SomeMethod()
+        {
+            Console.WriteLine("SomeMethod called.");
+        }
+    }
+
+}
